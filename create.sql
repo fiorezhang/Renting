3b54e6541578010fa21e0f9d929e1b204b09f276
fiorezhang <fiorezhang@sina.com>
2018-11-09 17:46:07 +0800

    add data pre-analysis

---
+++ create.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS `RENTING`;
+USE RENTING;
+#DROP TABLE `full`;
+CREATE TABLE IF NOT EXISTS `full`(
+	`company`    varchar(40),
+    `class`      varchar(40),
+    `date`       varchar(40),
+    `time`       varchar(40),
+    `province`   varchar(40),
+    `city`       varchar(40),
+    `district`   varchar(40),
+    `community`  varchar(40),
+    `money`      varchar(40),
+    `room`       varchar(40),
+    `area`       varchar(40),
+    `direction`  varchar(40),
+    `floor`      varchar(40),
+    `decoration` varchar(40),
+    `old`        varchar(40)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;

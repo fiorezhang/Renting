3a72863c571ddf6c3924328364da43a8eadf1075
fiorezhang <fiorezhang@sina.com>
2018-11-09 14:10:45 +0800

    change file name

--- load.py
+++ load.py
@@ -21,7 +21,7 @@ FOLDER_ORIGINAL = "ORG"
 FOLDER_CLEAN = "CLN"
 
 DATABASE = 'RENTING'
-TABLE = 'rent'
+TABLE = 'full'
 VARCHAR_MAX = 40
 
 def readCsvToSql(filename, database, table):
@@ -63,3 +63,37 @@ def importData():
 
 if __name__ == '__main__':
     importData()
+
+
+#附上SQL创建表格的代码，需要在执行导入python脚本之前创建好表    
+'''
+CREATE DATABASE IF NOT EXISTS `RENTING`;
+USE RENTING;
+#DROP TABLE `rent`;
+CREATE TABLE IF NOT EXISTS `rent`(
+	`company`    varchar(40),
+    `class`      varchar(40),
+    `date`       varchar(40),
+    `time`       varchar(40),
+    `province`   varchar(40),
+    `city`       varchar(40),
+    `district`   varchar(40),
+    `community`  varchar(40),
+    `money`      varchar(40),
+    `room`       varchar(40),
+    `area`       varchar(40),
+    `direction`  varchar(40),
+    `floor`      varchar(40),
+    `decoration` varchar(40),
+    `old`        varchar(40)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SELECT * FROM `rent` LIMIT 100;
+SELECT COUNT(*) FROM `rent`;
+SELECT COUNT(*) FROM `rent` WHERE `money` != 'NULL';
+SELECT `class`, COUNT(*) AS `count_class` FROM `rent` GROUP BY `class` ORDER BY `count_class` DESC;
+SELECT `company`, COUNT(*) AS `count_company` FROM `rent` GROUP BY `company` ORDER BY `count_company` DESC;
+SELECT `province`, COUNT(*) AS `count_province` FROM `rent` GROUP BY `province` ORDER BY `count_province` DESC;
+SELECT `city`, COUNT(*) AS `count_city` FROM `rent` GROUP BY `city` ORDER BY `count_city` DESC;
+SELECT `district`, COUNT(*) AS `count_district` FROM `rent` WHERE `district` != 'NULL' GROUP BY `district` ORDER BY `count_district` DESC LIMIT 100;
+SELECT `money`, COUNT(*) AS `count_money` FROM `rent` WHERE `money` != 'NULL' AND `money` != 'NOVA' GROUP BY `money` ORDER BY `money` DESC LIMIT 100;
+'''    
